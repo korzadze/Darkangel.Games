ea235d8fb447bf82e1e99ba2ba3a64ddcf574cbd
Александр Корзадзе <korzadze@outlook.com>
2021-04-21 14:10:31 +0300

    добавил контрол SpiceDensity как в игре

---
+++ Darkangel.Games.Dune92.SpiceDensityControl/SpiceDensity.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Darkangel.Games.Dune92
+{
+    /// <summary>
+    /// Логика взаимодействия для SpiceDensity.xaml
+    /// </summary>
+    public partial class SpiceDensity : UserControl
+    {
+        public SpiceDensity()
+        {
+            InitializeComponent();
+        }
+
+        private void Density00_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void Density01_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Density02_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Density03_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Density04_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Density05_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
